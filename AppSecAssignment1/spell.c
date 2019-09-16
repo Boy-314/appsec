df4ecc13c44ead985c6da688b3de5336e94b8bff
Boy-314 <boy.314.yee.willie@gmail.com>
2019-09-16 19:36:34 -0400

    started week2 assignment Signed

---
+++ AppSecAssignment1/spell.c
@@ -0,0 +1,8 @@
+#include "dictionary.h"
+
+bool check_word(const char* word, hashmap_t hashtable[])
+{
+	
+}
+bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]);
+int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[]);
