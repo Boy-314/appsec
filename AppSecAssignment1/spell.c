7f8cbab78de619dcf649c9c77abc28df7746fdf6
Boy-314 <boy.314.yee.willie@gmail.com>
2019-09-16 20:02:51 -0400

    working on week2 Signed

--- AppSecAssignment1/spell.c
+++ AppSecAssignment1/spell.c
@@ -1,8 +1,21 @@
+#include <ctype.h>
 #include "dictionary.h"
 
 bool check_word(const char* word, hashmap_t hashtable[])
 {
-	
+	int bucket = hash_function(word);
+	hashmap_t cursor = hashmap[bucket];
+	while(cursor != NULL)
+	{
+		char* lower_case;
+		if(word == cursor->word)
+		{
+			return 1;
+		}
+		// TODO: else if lower case of word = cursor->word, return 1
+		cursor = cursor->next;
+	}
+	return 0;
 }
 bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[]);
 int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[]);
