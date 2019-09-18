66c15831de90e8fd0697033f1d2b1d6ceb62ed74
Boy-314 <boy.314.yee.willie@gmail.com>
2019-09-18 15:32:15 -0400

    initial make, bug fixes, need to fix for loops and update for new pseudocode Signed

--- AppSecAssignment1/spell.c
+++ AppSecAssignment1/spell.c
@@ -1,12 +1,13 @@
 #include <ctype.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "dictionary.h"
 
 bool check_word(const char* word, hashmap_t hashtable[])
 {
 	int bucket = hash_function(word);
-	hashmap_t cursor = hashmap[bucket];
+	hashmap_t cursor = hashtable[bucket];
 	while(cursor != NULL)
 	{
 		// char* lower_case = lower_case(word)
@@ -14,7 +15,7 @@ bool check_word(const char* word, hashmap_t hashtable[])
 		char* lower_case = (char*)malloc(length + 1);
 		lower_case[length] = 0;
 		
-		for(int i = 0; i < length; i++) {lower[i] = tolower(word[i]);}
+		for(int i = 0; i < length; i++) {lower_case[i] = tolower(word[i]);}
 		if(word == cursor->word) {return 1;}
 		else if(lower_case == cursor->word) {return 1;}
 		cursor = cursor->next;
@@ -24,15 +25,17 @@ bool check_word(const char* word, hashmap_t hashtable[])
 
 bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[])
 {
+	// TODO: fix for loop
 	for(auto i : hashtable[]) {i = NULL;}
 	FILE* dict_file = fopen(dictionary_file,"r");
 	if(dict_file == NULL) {return 0;}
-	char* word[LENGTH];
+	char word[LENGTH];
 	while(fgets(word, LENGTH, dict_file) != NULL)
 	{
 		hashmap_t new_node;
 		new_node->next = NULL;
-		new_node->word = word;
+		strcpy(new_node->word, word);
+		// new_node->word = word;
 		int bucket = hash_function(word);
 		if(hashtable[bucket] == NULL) {hashtable[bucket] = new_node;}
 		else
@@ -41,7 +44,8 @@ bool load_dictionary(const char* dictionary_file, hashmap_t hashtable[])
 			hashtable[bucket] = new_node;
 		}
 	}
-	fclose(file);
+	fclose(dict_file);
+	return 1;
 }
 
 // additional method to remove punctuation from a word
@@ -52,19 +56,13 @@ void remove_punctuation(char* p)
 
     while (*src)
     {
-       if (ispunct((unsigned char)*src))
-       {
-          src++;
-       }
+       if (ispunct((unsigned char)*src)) {src++;}
        else if (src == dst)
        {
           src++;
           dst++;
        }
-       else
-       {
-          *dst++ = *src++;
-       }
+       else {*dst++ = *src++;}
     }
 
     *dst = 0;
@@ -73,15 +71,16 @@ void remove_punctuation(char* p)
 int check_words(FILE* fp, hashmap_t hashtable[], char* misspelled[])
 {
 	int num_misspelled = 0;
-	char* line[MAX_SIZE];
-	while(fgets(line, MAX_SIZE, fp) != NULL)
+	char* line;
+	while(fgets(line, HASH_SIZE, fp) != NULL)
 	{
 		char* split_line = strtok(line, " ");
 		int misspelled_index = 0;
+		// TODO: fix for loop
 		for(auto word : split_line)
 		{
 			word = remove_punctuation(word);
-			if(!check_word(word))
+			if(!check_word(word, hashtable))
 			{
 				misspelled[misspelled_index] = word;
 				misspelled_index = misspelled_index + 1;
